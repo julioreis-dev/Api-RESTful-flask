754cf036774c33fab64b21fff7d6713a0c91ed31
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2021-03-10 23:30:30 -0300

    teste

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from environs import Env
 
 app = Flask(__name__)
-
+# novo
 # ___________________________ Variabel Enviroment _______________________________________ #
 env = Env()
 env.read_env()
